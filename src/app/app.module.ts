7a1b280499570a5c321e5a30f2ff736ce6093efd
nolangh <nolanghiggins@gmail.com>
2022-10-08 15:18:11 -0600

    -a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './modules/app-routing.module';
 //import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './modules/material/material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,6 +26,7 @@ import { UserSelectionsComponent } from './components/user-selections/user-selec
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/services/game.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Game } from '../interfaces/game';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GameService {
+  constructor(private http: HttpClient) {}
+  searchByName(searchText: string): Observable<Game[]> {
+    const alteredText = searchText.replace(/\s/g, '+');
+    return this.http
+      .get<Game[]>(
+        `https://api.boardgameatlas.com/api/search?name=${alteredText}&client_id=${environment.boardgameAPI}`
+      )
+      .pipe(map((response) => response['games']));
+  }
+}
