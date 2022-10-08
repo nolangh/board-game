fe10100a544619fbf4013ce3ede25a79bd0d09e2
nolangh <nolanghiggins@gmail.com>
2022-10-08 14:42:34 -0600

    Finished Part-3/2

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'src/app/interfaces/menu-item';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
+  menuItems: MenuItem[] = [
+    {
+      name: 'Search',
+      route: '/search',
+      icon: 'search',
+    },
+    {
+      name: 'Owned',
+      route: '/owned',
+      icon: 'verified_outlined',
+    },
+    {
+      name: 'Wishlist',
+      route: '/wishlist',
+      icon: 'switch_access_shortcut',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule],
-  exports: [MatToolbarModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+  ],
+  exports: [MatToolbarModule, MatSidenavModule, MatListModule, MatIconModule],
 })
 export class MaterialModule {}
