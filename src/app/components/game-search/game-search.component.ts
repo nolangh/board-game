92e3dd3162a7e9409d468781f4065e1fb6dba6d8
nolangh <nolanghiggins@gmail.com>
2022-10-08 16:08:24 -0600

    -a

--- src/app/components/game-search/game-search.component.ts
+++ src/app/components/game-search/game-search.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ListType } from 'src/app/enums/list-type';
 import { Game } from 'src/app/interfaces/game';
 import { GameService } from 'src/app/services/game.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-game-search',
@@ -10,9 +13,24 @@ import { GameService } from 'src/app/services/game.service';
 export class GameSearchComponent implements OnInit {
   searchText!: string;
   games: Game[] = [];
-  constructor(private gameService: GameService) {}
+  ownedGames: { [id: string]: Game } = {};
+  wishListGames: { [id: string]: Game } = {};
+  constructor(
+    private gameService: GameService,
+    private localStorageService: LocalStorageService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.localStorageService.getGameList(ListType.OWNEDLIST);
+    this.localStorageService.getGameList(ListType.WISHLIST);
+
+    this.localStorageService.ownedGames.subscribe(
+      (games) => (this.ownedGames = _.mapKeys(games, 'id'))
+    );
+    this.localStorageService.wishListGames.subscribe(
+      (games) => (this.wishListGames = _.mapKeys(games, 'id'))
+    );
+  }
 
   search() {
     this.gameService
